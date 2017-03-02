066c8c20d38b04e808674be1fe7814ed8693c73f
Cody Fuller <git@codyfuller.com>
2017-03-02 01:04:37 -0600

    add an offset

--- Math_Roses/sketch.js
+++ Math_Roses/sketch.js
@@ -1,6 +1,9 @@
+// https://en.wikipedia.org/wiki/Rose_(mathematics)
+
 var k = 5.0/8.0;
 var n = 5;
 var d = 8;
+var offset = 100;
 var h = 100;
 
 function setup() {
@@ -10,6 +13,7 @@ function setup() {
   n = createSlider(1, 10, n, 0.1);
   d = createSlider(1, 10, d, 0.1);
   h = createSlider(0, 360, h, 1);
+  offset = createSlider(0, 300, offset, 1);
 }
 
 function draw() {
@@ -23,7 +27,7 @@ function draw() {
   stroke(h.value(), 100, 100);
   strokeWeight(0.7);
   for (var a = 0; a < TWO_PI * d.value(); a += 0.01){
-    var r = 250 * cos(k*a);
+    var r = 250 * cos(k*a) + offset.value();
     var x = r * cos(a);
     var y = r * sin(a);
     vertex(x, y);
