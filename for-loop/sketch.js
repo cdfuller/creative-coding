e962f23ee9716f9cbf07f1f3dbbfd43c1b308c1e
Cody Fuller <cody@nexthealthchoice.com>
2017-10-12 16:21:12 -0500

    Remove randomize functionality

--- for-loop/sketch.js
+++ for-loop/sketch.js
@@ -27,28 +27,18 @@ function drawGUI() {
     'BG color': [22, 18, 79],
     '# of lines': 10000,
     'Line weight': 0.2,
-    'Randomize': randomize,
     'Redraw': drawScreen,
   }
 
   let colors = gui.addFolder('Colors');
   colors.open();
-  colors.addColor(config, 'Line color').listen();
-  colors.addColor(config, 'BG color').listen();
+  colors.addColor(config, 'Line color');
+  colors.addColor(config, 'BG color');
 
   let lines = gui.addFolder('Lines');
   lines.open();
-  lines.add(config, '# of lines').listen();
-  lines.add(config, 'Line weight', 0.1, 5).step(0.1).listen();
+  lines.add(config, '# of lines').min(0).step(100);
+  lines.add(config, 'Line weight', 0.1, 5).step(0.1);
 
-  gui.add(config, 'Randomize');
   gui.add(config, 'Redraw');
-}
-
-function randomize(){
-  config['# of lines'] =  int(random(250, 5000));
-  config['Line weight'] =  random(0.1, 1.0);
-  config['Line color'] =  [random(0, 255), random(0, 255), random(0, 255)];
-  config['BG color'] =  [random(0, 255), random(0, 255), random(0, 255)];
-  drawScreen();
 }
