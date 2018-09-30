66404662a3447acd375c6989bbd169fdae32dca0
Cody Fuller <git@codyfuller.com>
2018-09-30 00:13:01 -0500

    Basic interactions

--- traffic/src/sketch.js
+++ traffic/src/sketch.js
@@ -1,5 +1,3 @@
-console.log("sketch loaded");
-
 // Factors of  1200:
 // 1, 2, 3, 4, 5, 6, 8, 10, 12, 15, 16, 20, 24, 25, 30, 40, 48, 50, 60, 75, 80, 100, 120, 150, 200, 240, 300, 400, 600, 1200
 //
@@ -7,6 +5,8 @@ const NOISE_SEED = 10003;
 const RANDOM_SEED = NOISE_SEED;
 const NUM_FRAMES = 5000;
 const DEBUG_MODE = false;
+
+const VEHICLE_COUNT = 5;
 
 let vehicles = [];
 
@@ -16,10 +16,14 @@ function setup() {
   noiseSeed(NOISE_SEED);
   randomSeed(RANDOM_SEED);
 
-  for (let i = 1; i <= 20; i++) {
-    let d = TWO_PI / 20 * i;
-    let v = new Vehicle(d);
+  for (let i = 0; i < VEHICLE_COUNT; i++) {
+    let d = 1000 / VEHICLE_COUNT * i;
+    let v = new Vehicle(d, i);
     vehicles.push(v);
+  }
+
+  for (let i = 0; i < vehicles.length; i++) {
+    vehicles[i].next = vehicles[(i + 1) % vehicles.length];
   }
 }
 
@@ -30,16 +34,47 @@ function draw() {
   noFill();
   ellipse(width / 2, height/2, 450, 450);
 
-  translate(width/2, height/2);
+  let positions = [];
   for ( v of vehicles ) {
     v.update();
-    v.draw();
+    positions.push(v.getPosition());
   } 
+  
+  push();
+  translate(width/2, height/2);
+  drawCircle(positions);
+  pop();
+  drawGraph(positions);
 
   if (DEBUG_MODE) {
     if (frameCount % 100 == 0) printStatus();
     if (frameCount == NUM_FRAMES) noLoop();
   }
+}
+
+function drawCircle(positions) {
+  let r = 225;
+  for (p of positions) {
+    let theta = translateCirclePosition(p);
+    let x = cos(theta) * r;
+    let y = sin(theta) * r;
+    ellipse(x, y, 10);
+  }
+}
+
+function translateCirclePosition(position) {
+  return position / 1000 * TWO_PI;
+}
+
+function drawGraph(positions) {
+  for ( p of positions) {
+    let x = translateGraphPosition(p);
+    line(x, height-10, x, height-30);
+  }
+}
+
+function translateGraphPosition(position) {
+  return position / 1000 * width;
 }
 
 function printStatus() {
--- traffic/src/vehicle.js
+++ traffic/src/vehicle.js
@@ -1,20 +1,32 @@
-const STATES = [ACCELERATING, BRAKING, STOPPED, COLLISION, CRUISING];
+const STATES = ["ACCELERATING", "BRAKING", "STOPPED", "COLLISION", "CRUISING"];
 
 class Vehicle {
-  constructor(position) {
+  constructor(position, index) {
     this.position = position;
-    this.speed = 0.007;
+    this.speed = 1.3;
     this.state = {};
+    this.next = null;
+    this.index = index;
   }
 
-  draw() {
-    let x = cos(this.position) * 225;
-    let y = sin(this.position) * 225;
-    ellipse(x, y, 10);
-  } 
+  // draw() {
+  //   let x = cos(this.position) * 225;
+  //   let y = sin(this.position) * 225;
+  //   ellipse(x, y, 10);
+  // } 
 
   update() {
-    this.position += this.speed;
+    let d = this.next.position - this.position;
+    if (d < 10) {
+      this.speed = 0.3;
+    } else {
+      this.speed += random() / 100 * 5;
+    }
+    this.position = (this.position + this.speed) % 1000;
+  }
+
+  getPosition() {
+    return this.position;
   }
 
 }
